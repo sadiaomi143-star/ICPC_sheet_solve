dbfcdd419a7f124a07f4195d3202553d3410441c
Sadia Alam Ome <sadiaomi143@gmail.com>
2026-01-17 09:05:59 +0600

    Create problem_Z.c

---
+++ sheet#1/problem_Z.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main() {
+    double A, B, C, D;
+    scanf("%lf %lf %lf %lf", &A, &B, &C, &D);
+ 
+    double left = B * log(A);
+    double right = D * log(C);
+ 
+    if (left > right)
+        printf("YES");
+    else
+        printf("NO");
+ 
+    return 0;
+}
