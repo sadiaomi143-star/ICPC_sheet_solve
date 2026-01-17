71fc0edbbd8ad02b4712a0e07b8815129b578e49
Sadia Alam Ome <sadiaomi143@gmail.com>
2026-01-17 08:40:08 +0600

    Create problem_B.c

---
+++ sheet#1/problem_B.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+ 
+int main()
+///int=10^9
+///long long =10^18
+{
+    int a;
+    long long int b ;
+    char c ;
+    float d;
+    double e;
+    scanf("%d %lld %c %f %lf",&a,&b,&c,&d,&e);
+ 
+ 
+    printf("%d\n",a);
+    printf("%lld\n",b);
+    printf("%c\n",c);
+    printf("%f\n",d);
+    printf("%lf",e);
+ 
+ 
+ 
