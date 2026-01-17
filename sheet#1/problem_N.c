53bc34415940154050ccaf8c80afec48b5512ab7
Sadia Alam Ome <sadiaomi143@gmail.com>
2026-01-17 08:59:58 +0600

    Create problem_N.c

---
+++ sheet#1/problem_N.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    char x;
+    scanf("%c",&x);
+ 
+    if(x>='A'&& x<='Z')
+ 
+ 
+ 
+    {
+        printf("%c",x+32);
+ 
+    }
+    else
+    {
+        printf("%c",x-32);
+    }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
