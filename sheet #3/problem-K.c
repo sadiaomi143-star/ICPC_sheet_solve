7b9758a43eb3afe79ce307594240907e1e7b7cc5
Sadia Alam Ome <sadiaomi143@gmail.com>
2026-01-17 13:34:01 +0600

    Create problem-K.c

---
+++ sheet #3/problem-K.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+    int n;
+    scanf("%d", &n);
+ 
+    char s[n + 1];     
+    scanf("%s", s);    
+ 
+    int sum = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        sum += s[i] - '0';  
+    }
+ 
+    printf("%d\n", sum);
+ 
+    return 0;
+}
