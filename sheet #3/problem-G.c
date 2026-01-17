f8fad50ed98e0aea9ee3c91833e3d43688462115
Sadia Alam Ome <sadiaomi143@gmail.com>
2026-01-17 13:32:23 +0600

    Create problem-G.c

---
+++ sheet #3/problem-G.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    long long a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%lld",&a[i]);
+    }
+    int first=0;
+    int last=n-1;
+    while(first<last)
+    {
+        if(a[first]!=a[last])
+        {
+            printf("NO\n");
+            return 0;
+        }
+        first++;
+        last--;
+    }
+    printf("YES\n");
+}
