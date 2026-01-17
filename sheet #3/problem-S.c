8f2a792f129c7bc2c6205d9938630413c3ebb59f
Sadia Alam Ome <sadiaomi143@gmail.com>
2026-01-17 13:35:05 +0600

    Create problem-S.c

---
+++ sheet #3/problem-S.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int main() {
+    int n, m, x;
+    scanf("%d %d", &n, &m);
+ 
+    int a[n][m];
+ 
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            scanf("%d", &a[i][j]);
+ 
+    scanf("%d", &x);
+ 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] == x) {
+                printf("will not take number");
+                return 0;
+            }
+        }
+    }
+ 
+    printf("will take number");
+    return 0;
+}
